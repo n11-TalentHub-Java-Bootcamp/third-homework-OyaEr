4c035572367e30e4b2ad17e782de7f3a9d82142c
OyaEr <oyaerdayi@gmail.com>
2021-12-25 01:35:47 +0300

    Created entities.

---
+++ src/main/java/com/oyaerdayi/springboot/ThirdHomeworkOyaErApplication.java
@@ -0,0 +1,13 @@
+package com.oyaerdayi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHomeworkOyaErApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdHomeworkOyaErApplication.class, args);
+    }
+
+}
